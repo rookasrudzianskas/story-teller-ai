ab64f3b0617a742e6f4a5ee65c1ff3e5165ac00f
rokas <rokas@rokas.com>
2024-06-28 08:56:36 +0300

    Add lucide-react icons to Header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Link from "next/link";
+import {BookOpen, FilePen} from "lucide-react";
 
 const Header = ({}) => {
   return (
@@ -16,8 +17,15 @@ const Header = ({}) => {
         </div>
       </Link>
 
-      <div className={''}>
-
+      <div className={'absolute -top-5 right-5 flex space-x-2 '}>
+        <Link href={'/'}>
+          <FilePen className={'w-8 h-8 lg:w-10 lg:h-10 mx-auto text-purple-500 mt-10 border border-purple-500 p-2' +
+            ' rounded-md hover:opacity-50 cursor-pointer'} />
+        </Link>
+        <Link href={'/stories'}>
+          <BookOpen className={'w-8 h-8 lg:w-10 lg:h-10 mx-auto text-purple-500 mt-10 border border-purple-500 p-2' +
+            ' rounded-md hover:opacity-50 cursor-pointer'} />
+        </Link>
       </div>
     </header>
   );
